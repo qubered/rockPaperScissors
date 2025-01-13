f849e715db492ecd77271ec8d2fd39750b843ced
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-13 16:54:10 +1100

    Add Logic to get User Choice and check its valid

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Hello, World!")
 
+choices = ["rock","paper","scissors"]
+
 function getComputerChoice() {
     choiceInt = Math.floor(Math.random() * 3);
     switch (choiceInt) {
@@ -14,4 +16,23 @@ function getComputerChoice() {
             break;
     }
     return choice;
+}
+
+function getUserChoice(gameCount, userScore, computerScore) {
+    rawChoice = prompt(`
+        Game: ${gameCount}
+        Score: 
+            You: ${userScore} 
+            Computer: ${computerScore}
+
+        Rock, Paper or Scissors?
+        `);
+    cleanChoice = rawChoice.toLowerCase()
+    if (choices.indexOf(cleanChoice) > -1) {
+        return cleanChoice;
+    }
+    else {
+        console.log("BAD USER INPUT") ;
+        return;
+    } 
 }
