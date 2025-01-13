284ab4863c51fca21a2605908512d7b84b471793
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-13 16:43:28 +1100

    Add Logic for Computer Choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello, World!")
+
+function getComputerChoice() {
+    choiceInt = Math.floor(Math.random() * 3);
+    switch (choiceInt) {
+        case 0: 
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+    }
+    return choice;
+}
